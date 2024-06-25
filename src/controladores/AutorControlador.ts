2b4bb847bfe19073ae476837f18a83e1f8ab1dcb
Vinicius Gabriel <viniciusxisto2014@hotmail.com>
2024-06-25 15:52:42 -0300

    implementação do editar autor

--- src/controladores/AutorControlador.ts
+++ src/controladores/AutorControlador.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { autores } from "../bancodedados";
+import Autor from "../modelos/Autor";
 
 export default class AutorControlador {
   listar(req: Request, res: Response) {
@@ -20,5 +21,48 @@ export default class AutorControlador {
     }
 
     return res.json(autor);
+  }
+
+  cadastrar(req: Request, res: Response) {
+    const { nome, idade } = req.body;
+
+    if (!nome || !idade) {
+      return res.status(400).json({
+        mensagem: "O nome e a idade do autor são obrigtórios!",
+      });
+    }
+
+    const autor = new Autor({
+      nome,
+      idade,
+    });
+    autores.push(autor);
+
+    return res.status(201).json(autor);
+  }
+
+  editar(req: Request, res: Response){
+    const { id } = req.params;
+    const { nome, idade } = req.body;
+
+    if (!nome || !idade) {
+      return res.status(400).json({
+        mensagem: "O nome e a idade do autor são obrigtórios!",
+      });
+    }
+
+    const autor = autores.find((elemento) => {
+      return elemento.id === id;
+    });
+
+    if (!autor) {
+      return res.status(404).json({
+        mensagem: "Autor não encontrado!",
+      });
+    }
+    autor.nome = nome
+    autor.idade = idade 
+
+    return res.status(204).send()
   }
 }
