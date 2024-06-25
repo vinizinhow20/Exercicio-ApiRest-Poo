680a0033535fe206372b065395ff7f9f09d2fada
Vinicius Gabriel <viniciusxisto2014@hotmail.com>
2024-06-25 14:44:08 -0300

    imprementação modelo postagem

---
+++ src/modelos/Post.ts
@@ -0,0 +1,30 @@
+import { v4 as uuidv4 } from 'uuid'
+import Autor from './Autor'
+
+type TPost = {
+    titulo:string
+    descricao:string
+    autor: Autor
+}
+
+
+export default class Post {
+    readonly id:string
+    titulo:string
+    descricao:string
+    autor: Autor
+    data_criacao: Date
+
+    constructor(post:TPost){
+        this.id = this.gerarId()
+        this.titulo = post.titulo
+        this.descricao = post.descricao
+        this.autor = post.autor
+        this.data_criacao = new Date
+        
+    }
+
+    private gerarId():string{
+        return uuidv4()
+    } 
+}
