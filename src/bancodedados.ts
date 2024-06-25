c35c80b33486b31fca84b9612ec37df4df60c902
Vinicius Gabriel <viniciusxisto2014@hotmail.com>
2024-06-25 16:44:17 -0300

    imprementação do Listar postagens

--- src/bancodedados.ts
+++ src/bancodedados.ts
@@ -2,5 +2,14 @@ import Autor from "./modelos/Autor";
 import Post from "./modelos/Post";
 
 export const autores: Autor[] = []
-export const posts: Post[] = []
+export const posts: Post[] = [
+    new Post({
+        titulo: 'meu primeiro post',
+        descricao: 'Descrição do meu primeiro post',
+        autor: new Autor({
+            nome: 'Vinicius Gabriel',
+            idade: 25
+        })
+    })
+]
 
---
+++ src/controladores/PostControlador.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { autores, posts } from "../bancodedados";
+import Autor from "../modelos/Autor";
+
+export default class PostControlador {
+  listar(req: Request, res: Response) {
+    return res.json(posts);
+  }
+
+  detalhar(req: Request, res: Response) {
+   
+  }
+
+  cadastrar(req: Request, res: Response) {
+    
+  }
+
+  editar(req: Request, res: Response){
+  }
+    
+  excluir(req: Request, res: Response){
+    
+  }
+}
