b5356a1ceb575040cad86011f47fd3822a6c3c63
Vinicius Gabriel <viniciusxisto2014@hotmail.com>
2024-06-25 20:57:32 -0300

    implementação do deletar postagem

--- src/rotas.ts
+++ src/rotas.ts
@@ -1,21 +1,21 @@
-import { Router } from 'express'
-import AutorControlador from './controladores/AutorControlador'
-import PostControlador from './controladores/PostControlador'
+import { Router } from "express";
+import AutorControlador from "./controladores/AutorControlador";
+import PostControlador from "./controladores/PostControlador";
 
-const rotas = Router()
+const rotas = Router();
 
-const autorControlador = new AutorControlador()
-const postControlador = new PostControlador()
+const autorControlador = new AutorControlador();
+const postControlador = new PostControlador();
 
-rotas.get('/autores', autorControlador.listar) 
-rotas.get('/autores/:id', autorControlador.detalhar) 
-rotas.post('/autores', autorControlador.cadastrar) 
-rotas.put('/autores/:id', autorControlador.editar) 
-rotas.delete('/autores/:id', autorControlador.excluir) 
+rotas.get("/autores", autorControlador.listar);
+rotas.get("/autores/:id", autorControlador.detalhar);
+rotas.post("/autores", autorControlador.cadastrar);
+rotas.put("/autores/:id", autorControlador.editar);
+rotas.delete("/autores/:id", autorControlador.excluir);
 
-rotas.get('/postagens', postControlador.listar) 
-rotas.get('/postagens/:id', postControlador.detalhar) 
-rotas.post('/postagens', postControlador.cadastrar )
-rotas.patch('/postagens/:id', postControlador.editar )
-export default rotas 
-
+rotas.get("/postagens", postControlador.listar);
+rotas.get("/postagens/:id", postControlador.detalhar);
+rotas.post("/postagens", postControlador.cadastrar);
+rotas.patch("/postagens/:id", postControlador.editar);
+rotas.delete("/postagens/:id", postControlador.excluir);
+export default rotas;
